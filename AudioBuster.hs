b86b7fa9a79a770904b6cadbc3de7cf399611f83
HaskellHegemonie <znerol@mail.de>
2024-05-20 23:20:03 +0200

    Cleanup in AudioBuster

--- AudioBuster.hs
+++ AudioBuster.hs
@@ -1,22 +1,19 @@
 module Ohrenkrebs where
 import Lib
--- "doas modprobe snd_aloop"
--- "pactl list"
--- "pactl load-module module-loopback"
--- "pactl load-module module-null-sink"
 
 
 name = "createdSinks"
 sinkOp = openFile name ReadWriteMode
+execPactlOut flags = createProcessOut $ proc "pactl" flags
+listJsonFlag name = ["-f", "json", "list", name]  
 
 main = do
   sinkH <- sinkOp
-  nullSink <- createProcessOut (proc "pactl" ["load-module", "module-null-sink"])
-  listSinks <- createProcessOut (proc "pactl" ["list", "sinks"])
-  listSources <- createProcessOut (proc "pactl" ["list", "sources"])
-  listSinkInputs <- createProcessOut (proc "pactl" ["list", "sink-inputs"])
-  listSourceOutputs <- createProcessOut (proc "pactl" ["list", "source-outputs"])
-  putStr listSinkInputs
+  nullSink <- execPactlOut ["load-module", "module-null-sink"]
+  s@[sinks, sources, sinkInputs, sinkOutputs] <- mapM (execPactlOut . listJsonFlag) $ words "sinks sources sink-inputs source-outputs"
+
+  mapM_ (appendFile "parsingNeeded". (<> "\n")) s
+
   hAppend sinkH nullSink hPutStr
   hClose sinkH
   pure ()
@@ -25,7 +22,7 @@ main = do
 cleanup = do
   sinkH <- sinkOp
   openSinks <- lines <$> hGetContents sinkH
-  forM_ openSinks (\sink -> putStrLn ("deleted " <> sink) >> createProcess (proc "pactl" ["unload-module", sink]))
+  forM_ openSinks (\sink -> putStrLn ("deleted " <> sink) >> createProcess (proc "pactl" [ "unload-module", sink]))
   hClose sinkH
   clearFileWithName name
 
